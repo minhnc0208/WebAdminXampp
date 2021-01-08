173ae8b68d042ff3c663edc1c9ba3304fbd0f0b1
minhlunscs <minhncps08611@fpt.edu.vn>
2021-01-08 13:27:28 +0700

    finish thong ke

--- admin/index.php
+++ admin/index.php
@@ -28,7 +28,7 @@ include('../database.inc.php');
 
 				$row = mysqli_num_rows($query_run);
 
-				echo '<h4> ' .$row. ' Users </h4> ';
+				echo '<h4> ' .$row. ' USERS </h4> ';
 			 ?>
 
 		  
@@ -50,8 +50,20 @@ include('../database.inc.php');
 	<div class="card-body">
 	  <div class="row no-gutters align-items-center">
 		<div class="col mr-2">
-		  <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-		  <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+		  <div class="text-xs font-weight-bold text-success text-uppercase mb-1">TOTAL FOODS</div>
+		  <br>
+		  <div class="h5 mb-0 font-weight-bold text-gray-800">
+		  <?php
+				
+				$query = "SELECT id from foods ORDER BY id";
+				$query_run = mysqli_query($con,$query);
+
+
+				$row = mysqli_num_rows($query_run);
+
+				echo '<h4> ' .$row. ' FOODS </h4> ';
+			 ?>
+		  </div>
 		</div>
 		<div class="col-auto">
 		  <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -67,16 +79,29 @@ include('../database.inc.php');
 	<div class="card-body">
 	  <div class="row no-gutters align-items-center">
 		<div class="col mr-2">
-		  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+		  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">TOTAL TYPE FOODS</div>
+		 	<br>
 		  <div class="row no-gutters align-items-center">
 			<div class="col-auto">
-			  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+			  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+			  
+			  <?php
+				
+				$query = "SELECT id from typefoods ORDER BY id";
+				$query_run = mysqli_query($con,$query);
+
+
+				$row = mysqli_num_rows($query_run);
+
+				echo '<h4> ' .$row. ' TYPE FOODS </h4> ';
+			 ?>
+			  </div>
 			</div>
-			<div class="col">
+			<!-- <div class="col">
 			  <div class="progress progress-sm mr-2">
 				<div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
 			  </div>
-			</div>
+			</div> -->
 		  </div>
 		</div>
 		<div class="col-auto">
@@ -93,8 +118,20 @@ include('../database.inc.php');
 	<div class="card-body">
 	  <div class="row no-gutters align-items-center">
 		<div class="col mr-2">
-		  <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-		  <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+		  <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">TOTAL PAYMENT METHODS</div>
+		  <br>
+		  <div class="h5 mb-0 font-weight-bold text-gray-800">
+		  <?php
+				
+				$query = "SELECT id from paymentmethods ORDER BY id";
+				$query_run = mysqli_query($con,$query);
+
+
+				$row = mysqli_num_rows($query_run);
+
+				echo '<h4> ' .$row. ' PAYMENT METHODS </h4> ';
+			 ?>
+		  </div>
 		</div>
 		<div class="col-auto">
 		  <i class="fas fa-comments fa-2x text-gray-300"></i>
